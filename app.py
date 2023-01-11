9413bdda44cb770e10d6e2e08670b2e8f4ab28b8
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-01-11 17:44:02 +0900

    Feat: 채팅 서버 구현

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
+httpRequest = request
+from flask_socketio import SocketIO, send
+
 import bcrypt
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.rvhpcnz.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
+socketIo = SocketIO(app)
 
 @app.route('/')
 def home():
@@ -28,12 +32,25 @@ def signIn():
 def chatRoom():
     return render_template('chat_room.html')
 
+@socketIo.on("message")
+def request(message):
+    print("message : " + message)
+    to_client = dict()
+    if message == 'new_connect':
+        to_client['message'] = "새로운 유저가 난입하였다!!"
+        to_client['type'] = 'connect'
+    else:
+        to_client['message'] = message
+        to_client['type'] = 'normal'
+    # emit("response", {'data': message['data'], 'username': session['username']}, broadcast=True)
+    send(to_client, broadcast=True)
+
 
 @app.route('/signUp/give', methods=["POST"])
 def signUpPost():
-    idReceive = request.form["idGive"]
-    nameReceive = request.form["nameGive"]
-    passwordReceive = request.form["passwordGive"]
+    idReceive = httpRequest.form["idGive"]
+    nameReceive = httpRequest.form["nameGive"]
+    passwordReceive = httpRequest.form["passwordGive"]
 
     hashedPassword = bcrypt.hashpw(passwordReceive.encode('utf-8'), bcrypt.gensalt())
     hashedPassword = hashedPassword.decode()
@@ -56,8 +73,9 @@ def signUpGet():
 
 @app.route('/signIn/give', methods=["POST"])
 def signInGive():
-    idReceive = request.form["idGive"]
-    passwordReceive = request.form["passwordGive"]
+    print(httpRequest, type(httpRequest))
+    idReceive = httpRequest.form["idGive"]
+    passwordReceive = httpRequest.form["passwordGive"]
 
     user = list(db.users.find({'id': idReceive}, {'_id': False}))
     if len(user) > 0 and bcrypt.checkpw(passwordReceive.encode('utf-8'), user[0]['password'].encode('utf-8')):
@@ -72,8 +90,8 @@ def signInGive():
 # //comment 관리
 @app.route("/noticeBoard/post", methods=["POST"])
 def commentPost():
-    commentReceive = request.form["commentGive"]
-    nameReceive = request.form["nameGive"]
+    commentReceive = httpRequest.form["commentGive"]
+    nameReceive = httpRequest.form["nameGive"]
     doc = {
         'comment' : commentReceive,
         'name' : nameReceive
